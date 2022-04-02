45b50d4cdfc400b3561551f5c26c365fa304bf4e
SungDongIn <ddongin10@naver.com>
2022-04-02 21:17:53 +0900

    #2.2~2.4

--- src/index.js
+++ src/index.js
@@ -7,26 +7,72 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE TODO";
 
+const addToDo = (text) => {
+    return {
+        type: ADD_TODO,
+        text
+    }
+}
+
+const deleteToDo = (id) => {
+    return {
+        type: DELETE_TODO,
+        id
+    }
+}
+
 const reducer = (state = [], action) => {
-    //console.log(action);
+
     switch (action.type) {
         case ADD_TODO:
-            return [...state, { text: action.text, id: Date.now() }];
+            const newToDoObj = { text: action.text, id: Date.now() };
+            return [newToDoObj, ...state];
         case DELETE_TODO:
-            return [];
+            const newState = state.filter( toDo => toDo.id !== Number(action.id));
+            return newState;
         default:
             return state;
     }
 }
 const store = createStore(reducer);
 
-store.subscribe(() => { console.log(store.getState()) });
+const dispatchAddToDo = text => {
+    store.dispatch(addToDo(text));
+}
+
+const dispatchDeleteTodo = (event) => {
+    const id = event.target.parentNode.id;
+    store.dispatch(deleteToDo(id));
+}
+
+
+const paintToDos = () => {
+
+    const toDos = store.getState();
+    ul.innerHTML = ""; // ul 초기화
+    toDos.forEach( toDo => {
+        const li = document.createElement("li");
+        const btn = document.createElement("button");
+        btn.innerText = "❌"
+        btn.addEventListener("click", dispatchDeleteTodo);
+        li.id = toDo.id;
+        li.innerText = toDo.text;
+        li.appendChild(btn);
+        ul.appendChild(li);
+    });
+}
+store.subscribe(paintToDos);
+
 
 const createTodo = () => {
-    //console.log(inputTodo.value);
+
     const toDo = inputTodo.value;
+    if(toDo === "") {
+        return;
+    }
     inputTodo.value = "";
-    store.dispatch({ type: ADD_TODO, text: toDo });
+    inputTodo.focus();
+    dispatchAddToDo(toDo);
 }
 
 inputTodoBtn.addEventListener("click", createTodo);
